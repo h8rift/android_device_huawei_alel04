0d0c9fea878abbec579189a8c1eb17ada89f04e2
Shane Passmore <h8rift@gmail.com>
2016-01-31 16:04:17 -0600

    alel04: lets get this back to CM
    Change-Id: I28666e51d9f961e313f6d45322032684c9da4cbf

--- cm.mk
+++ cm.mk
@@ -14,8 +14,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -24,16 +22,16 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, device/huawei/alel04/full_alel04.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/alel04/alel04.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := alel04
-PRODUCT_NAME := carbon_alel04
+PRODUCT_NAME := cm_alel04
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=ALEL04 \
-    BUILD_FINGERPRINT=Huawei/ALE-L04/hwALE-Q:5.1/HuaweiALE-L04/C567B309:user/release-keys \
-    PRIVATE_BUILD_DESC="ALE-L04-user 5.1 GRJ90 C567B309 release-keys"
+	PRODUCT_NAME=ALEL04 \
+	BUILD_FINGERPRINT="Huawei/ALE-L04/hwALE-Q:5.1/HuaweiALE-L04/C567B309:user/release-keys" \
+	PRIVATE_BUILD_DESC="ALE-L04-user 5.1 GRJ90 C567B309 release-keys"
